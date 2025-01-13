9980faad2b5db6690f5266203ebd91c194381add
Julia Healey-Parera <jh884@duke.edu>
2025-01-12 23:30:02 -0500

    Fixed legend size

--- burner.R
+++ burner.R
@@ -8,11 +8,47 @@ coords2 <- reprocess_coords(obj2) |>
          V1 = V1 + .3,
          V2 = V2 + .3)
 
-ggplot() + 
+circle <- base_circle(1.8, 9, .1) 
+
+add_deviation_to_circle <- function(circle, deviation_factor) {
+  deviation_x <- runif(nrow(circle), -deviation_factor, deviation_factor)
+  deviation_y <- runif(nrow(circle), -deviation_factor, deviation_factor)
+  circle$x <- circle$x + deviation_x
+  circle$y <- circle$y + deviation_y
+  return(circle)
+}
+
+adjust_perimeter <- function(circle, target_perimeter) {
+  perimeter <- sum(sqrt(diff(circle$x)^2 + diff(circle$y)^2))
+  scale_factor <- target_perimeter / perimeter
+  circle$x <- circle$x * scale_factor
+  circle$y <- circle$y * scale_factor
+  return(circle)
+}
+
+base_circle <- function(radius, target_perimeter, deviation_factor) {
+  circle <- generate_circle(radius)
+  circle <- add_deviation_to_circle(circle, deviation_factor)
+  circle <- adjust_perimeter(circle, target_perimeter)
+  return(circle)
+}
+
+ggplot() +
+  geom_polygon(circle, mapping = aes(x=x, y=y), fill = "grey90") +
   geom_polygon(data = coords1, aes(x = V1, y = V2, fill = damage)) +
   geom_polygon(data = coords2, aes(x = V1, y = V2, fill = damage)) +
   scale_fill_distiller(direction = -1, palette = "Blues", limits = c(0, 100)) +
-  theme_void()
+  labs(fill = "Damage") +
+  theme_void() +
+  theme(legend.key.size = unit(.7, 'cm'), 
+        legend.text = element_text(size = 11), 
+        legend.title = element_text(size = 15))
+  
+map_col <- map_col + 
+  theme_void() +  
+  theme(plot.background = element_rect(fill = "white", color = NA),
+        theme(legend.key.size = unit(2, 'cm'), 
+              legend.text 
 
 ggplot() + 
   geom_polygon(data = coords1, aes(x = V1, y = V2, fill = damage)) +
